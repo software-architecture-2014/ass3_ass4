e7ef992da727945cac53f86072c94df10814f37f
David Wittenbrink <david.wittenbrink@gmail.com>
2014-12-29 14:19:44 +0100

    Added csv parsing and db writing 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#-*- coding: UTF-8 -*-
 import webapp2
 import logging
+import codecs
 from google.appengine.ext import ndb
 
 
@@ -25,5 +27,29 @@ class FrontPage(MainHandler):
   def get(self):
     self.response.out.write('Hello World!')
 
+class Erstelle(MainHandler):
+#20911104    15.4526367    47.0593698    Moserhofgasse
+  
+  def encode_list(self, l):
+    return [ codecs.encode(word, 'utf-8') for word in l ]
 
-app = webapp2.WSGIApplication([('/', FrontPage)])
+  def get(self):
+    ndb.delete_multi( Stops.query().fetch(keys_only=True) )
+
+    all_stops = []
+
+    with codecs.open('test.csv', 'r', 'utf-8') as input_file:
+      all_lines = input_file.readlines()
+      for line in all_lines[1:]:
+
+        words = line.split()
+        stop = Stops(stop_id = words[0],
+                     location = ndb.GeoPt(", ".join( words[1:3][::-1])),
+                     name = " ".join(self.encode_list(words[3:])))
+
+        all_stops.append(stop)
+
+    ndb.put_multi(all_stops)
+
+app = webapp2.WSGIApplication([('/', FrontPage),
+                               ('/erstelle', Erstelle)])
