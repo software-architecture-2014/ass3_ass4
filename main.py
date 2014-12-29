1493ab02167f6c8b8a8d0a9ccc021319bd563262
Franco Nieddu <franco.nieddu@student.tugraz.at>
2014-12-29 13:22:29 +0100

    start

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ import logging
 from google.appengine.ext import ndb
 
 
+class Stops(ndb.Model):
+    stop_id  = ndb.StringProperty(required = True)
+    created = ndb.DateTimeProperty(auto_now_add = True)
+    name = ndb.StringProperty(required = True)
+    location = ndb.GeoPtProperty(required = True)
+
+class Routes(ndb.Model):
+    routes_id = ndb.StringProperty(required = True)
+    name = ndb.StringProperty(required = True)
+
+class Mapping(ndb.Model):
+    stop_id = ndb.StringProperty(required = True)
+    routes_id = ndb.StringProperty(required = True)
 
 class MainHandler(webapp2.RequestHandler):
   pass
